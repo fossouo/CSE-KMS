06cb993133f164cc83ac78eeb6eb334821e56f18
fossouo <fossouodonald@gmail.com>
2020-07-02 23:37:41 +0200

    update README & python code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # CSE-KMS
+
+1. Install crypto libs
+
+pip install wheel 
+pip install paramiko
+pip install Crypto
+pip install s3-encryption
+pip install requests
+pip install cryptography
+
+2. Install aws encryption sdk 
+
+pip install aws-encryption-sdk-cli
+pip install aioboto3
+pip install asyncio
+pip install s3-encryption
+pip install aiohttp
+
---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+import aioboto3
+import csv
+from aioboto3.s3.cse import S3CSE, KMSCryptoContext
+
+async def main():
+    ctx = KMSCryptoContext(keyid='XXX-XXXXX-XXXXX', kms_client_args={'region_name': 'us-east-1'})
+
+#    some_data = b'2718,CA-2011-100006,9/7/2011,9/13/2011,1,DK-13375,101,TEC-PH-10002075,377.97,1001,3,0,109.6113'
+    some_data = open("orders.csv","r", encoding="utf-8").readlines()
+    
+    async with S3CSE(crypto_context=ctx, s3_client_args={'region_name': 'us-east-1'}) as s3_cse:
+        # Upload some binary data
+        await s3_cse.put_object(
+            Body=some_data,
+            Bucket='bucketname-XXXX',
+            Key='input/orders.csv.enc',
+        )
+
+        response = await s3_cse.get_object(
+            Bucket='bucketname-XXXX',
+            Key='input/orders.csv.enc',
+        )
+        data = await response['Body'].read()
+        print(list(data))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
