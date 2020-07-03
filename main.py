43acf850c7ebc2ffba3343bc2eea3084c54be687
fossouo <fossouodonald@gmail.com>
2020-07-03 09:35:56 +0200

    update README & python code

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from aioboto3.s3.cse import S3CSE, KMSCryptoContext
 async def main():
     ctx = KMSCryptoContext(keyid='XXX-XXXXX-XXXXX', kms_client_args={'region_name': 'us-east-1'})
 
-#    some_data = b'2718,CA-2011-100006,9/7/2011,9/13/2011,1,DK-13375,101,TEC-PH-10002075,377.97,1001,3,0,109.6113'
-    some_data = open("orders.csv","r", encoding="utf-8").readlines()
-    
+#   FOR STRING ENCRYPTION 
+
+    some_data = b'2718,CA-2011-100006,9/7/2011,9/13/2011,1,DK-13375,101,TEC-PH-10002075,377.97,1001,3,0,109.6113'
+
+#   FOR FILES ENCRYPTION 
+
+#   some_data = open('orders_noheaders.csv','rb')    
+
     async with S3CSE(crypto_context=ctx, s3_client_args={'region_name': 'us-east-1'}) as s3_cse:
         # Upload some binary data
         await s3_cse.put_object(
